cf34aa01b0911cfe7f10646bb917ccc4f852b5ff
Vacbo <peportella11@gmail.com>
2023-03-02 15:56:44 -0300

    receber setUp projeto4

--- receber.py
+++ receber.py
@@ -1,5 +1,8 @@
 from enlace import enlace
 import time
+from datetime import datetime
+from os import getcwd
+from os.path import basename
 
 # voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
 #   para saber a sua porta, execute no terminal :
@@ -11,14 +14,32 @@ SERIAL_PORT_NAME = "/dev/ttyACM1"            # Ubuntu (variacao de)
 # SERIAL_PORT_NAME = "/dev/tty.usbmodem1411"   # Mac    (variacao de)
 # SERIAL_PORT_NAME = "COM5"                    # Windows(variacao de)
 
-# HEAD format PayloadSize 2 Byte + 5 Byte PacketNumber + 5 Bytes TotalPackets
-# Payload 0 - 50 Bytes, the packet data.
-# # End of packet 3 Bytes (0xDD 0xEE 0xFF)
+LOG_FILE = getcwd() + "/logs/" + basename(__file__) + ".log"
 
-PACKET_END = b'\xDD\xEE\xFF'
+def log(msg : str) -> None:
+    with open(LOG_FILE, "a", encoding='utf-8') as file:
+        file.write(datetime.now().strftime('[%d/%m/%Y %H:%M:%S] ') + msg)
+        print(msg)
 
-HANDSHAKE = int(0).to_bytes(length=2, byteorder='big') + int(0).to_bytes(
-    length=5, byteorder='big') + int(0).to_bytes(length=5, byteorder='big') + PACKET_END
+# HEAD format h0 + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9
+# h0 – Tipo de mensagem  # h1 – Se tipo for 1: número do servidor Qualquer outro tipo: livre # h2 – Livre # h3 – Número total de pacotes do arquivo
+# h4 – Número do pacote sendo enviado # h5 – Se tipo for handshake: id do arquivo (crie um para cada arquivo). Se tipo for dados: tamanho do payload.
+# h6 – Pacote solicitado para recomeço quando a erro no envio # h7 – Ùltimo pacote recebido com sucesso #h8 – h9 – CRC (Por ora deixe em branco. Fará parte do projeto 5).
+# Payload 0 - 114 Bytes, the packet data.
+# End of packet 4 Bytes (0xAA 0xBB 0xCC 0xDD)
+
+PACKET_END = b'\xAA\xBB\xCC\xDD'
+
+PACKET_TYPE1_HANDSHAKE = b'\x01'
+PACKET_TYPE2_SERVER_LIVRE = b'\x02'
+PACKET_TYPE3_DATA = b'\x03'
+PACKET_TYPE4_VALIDATION = b'\x04'
+PACKET_TYPE5_TIMEOUT = b'\x05'
+PACKET_TYPE6_ERROR = b'\x06'
+
+SERVER_ID = b'\x45'
+
+HANDSHAKE = PACKET_TYPE1_HANDSHAKE + SERVER_ID + int(0).to_bytes(length=1, byteorder='big') + PACKET_END
 
 CONFIRMATION_PACKET = int(0).to_bytes(length=2, byteorder='big') + int(1).to_bytes(
     length=5, byteorder='big') + int(1).to_bytes(length=5, byteorder='big') + PACKET_END
@@ -29,20 +50,21 @@ def confirmHandshake(com: enlace) -> bool:
 
     print("Waiting for handshake...")  # DEBUG
 
-    handshake, _ = com.getData(12)
+    handshake, _ = com.getData(10)
 
     print("Handshake received.")  # DEBUG
 
     # Reads the handshake packet
-    payloadSize = int.from_bytes(handshake[:2], byteorder='big')
-    totalPackets = int.from_bytes(handshake[2:7], byteorder='big')
-    packetNumber = int.from_bytes(handshake[7:], byteorder='big')
+    msgType = int.from_bytes(handshake[:1], byteorder='big')
+    serverId = int.from_bytes(handshake[1:2], byteorder='big')
+    totalPackets = int.from_bytes(handshake[2:3], byteorder='big')
+    archiveId = int.from_bytes(handshake[4:5], byteorder='big')
 
     end, _ = com.getData(3)
     com.rx.clearBuffer()
 
     # Checks if the handshake is valid
-    if payloadSize == 0 and packetNumber == 0 and totalPackets == 0 and end == PACKET_END:
+    if msgType == 1 and serverId != SERVER_ID and totalPackets == 0 and end == PACKET_END:
         print("Handshake received is valid.")
         # Send handshake confirmation
         com.sendData(HANDSHAKE)
