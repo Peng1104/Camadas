666a81716d8512d141c90e858d8868a11cc4d963
Vacbo <peportella11@gmail.com>
2023-03-23 15:40:45 -0300

    First version of client.cpp

---
+++ Projeto-6/src/client.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+// Constants
+const int digitalTxPin = 5; // Digital pin that emulates the Tx pin
+const char asciiLetter = 'A'; // Choose any letter from the ASCII table to send
+const long baudRate = 9600;   // Chose any baud rate you want
+const long bitDelay = 1000000L / baudRate; // Calculate the delay between bits
+
+void setup() {
+  pinMode(digitalTxPin, OUTPUT); // Sets the digital pin as an output to emulate the Tx pin
+  digitalWrite(digitalTxPin, HIGH); // Sets the starting value of the digital pin to HIGH
+}
+
+void loop() {
+  sendSerialByte(asciiLetter); // Send the ASCIIletter each second
+  delay(1000);
+}
+
+void sendSerialByte(char data) {
+  // Start bit
+  digitalWrite(digitalTxPin, LOW);
+  delayMicroseconds(bitDelay);
+
+  // Data bits
+  int numberOfOnes = 0;
+  for (byte mask = 0x01; mask != 0; mask <<= 1) {
+    if (data & mask) {
+      digitalWrite(digitalTxPin, HIGH);
+      numberOfOnes++;
+    } else {
+      digitalWrite(digitalTxPin, LOW);
+    }
+    delayMicroseconds(bitDelay);
+  }
+
+  // Parity bit
+  if (numberOfOnes % 2 == 0) {
+    digitalWrite(digitalTxPin, LOW); // Even parity
+    delayMicroseconds(bitDelay);
+  }
+
+  // Stop bit
+  digitalWrite(digitalTxPin, HIGH);
+}
