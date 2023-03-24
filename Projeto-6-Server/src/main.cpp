6ec3e2e526c347134df8a8f4640172982d5caa92
Vacbo <pedrobp1@al.insper.edu.br>
2023-03-24 18:10:15 -0300

    Interuptions

--- Projeto-6-Server/src/main.cpp
+++ Projeto-6-Server/src/main.cpp
@@ -48,3 +48,33 @@ void loop() {
   }
   delay(1000);
 }
+
+/* Ref: https://electronoobs.com/eng_arduino_tut140.php
+Calculations for the timer: 
+  System clock 16 Mhz and Prescalar 64;
+  Timer 1 speed = 16Mhz/256 = 250 Khz    
+  Pulse time = 1/250 Khz =  4us  
+  Count up to = (1/bitDelay) / 4us = ?
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(digitalRxPin, INPUT); // Sets the digital pin as an output to emulate the Rx pin
+  cli(); // Disable interrupts 
+  TCCR1A = 0;                 
+  TCCR1B = 0;                 
+  TCCR1B |= B00000011;      
+  TIMSK1 |= B00000010;
+  OCR1A = bitDelay/4e-6;
+  sei();  //allow interrupts        
+}
+
+void loop() {
+....
+}
+
+ISR(TIMER1_COMPA_vect){
+  TCNT1  = 0; //Reset the timer for next interrupt
+  ...
+}
+*/ 
