abba6b2a03701d6354fd3f084f09d479fec4417d
Vacbo <peportella11@gmail.com>
2023-04-14 17:07:48 -0300

    Interrupt Iteration first version

--- Projeto-6-Client/src/main.cpp
+++ Projeto-6-Client/src/main.cpp
@@ -1,48 +1,81 @@
 #include <Arduino.h>
+#define TX_PIN 5
+#define BAUD_RATE 9600
+#define BIT_PERIOD (1000000 / BAUD_RATE)
 
-// Constants
-const int digitalTxPin = 5; // Digital pin that emulates the Tx pin
-const char asciiLetter = 'A'; // Choose any letter from the ASCII table to send
-const long baudRate = 9600;   // Chose any baud rate you want
-const long bitDelay = 1000000L / baudRate; // Calculate the delay between bits
+volatile uint8_t bitToSend = 0;
+volatile uint8_t byteToSend = 'A';
+volatile bool transmissionInProgress = false;
+volatile uint8_t numberOfOnes = 0;
 
 void setup() {
-  pinMode(digitalTxPin, OUTPUT); // Sets the digital pin as an output to emulate the Tx pin
-  digitalWrite(digitalTxPin, HIGH); // Sets the starting value of the digital pin to HIGH
+  Serial.begin(9600);
+  pinMode(TX_PIN, OUTPUT);
+  digitalWrite(TX_PIN, HIGH);
+
+  // Configure Timer1 for CTC (Clear Timer on Compare) mode and enable interrupt on OCR1A match
+  TCCR1A = 0;
+  TCCR1B = (1 << WGM12) | (1 << CS11); // Set CTC mode, prescaler = 8
+  TIMSK1 = (1 << OCIE1A); // Enable compare match interrupt for OCR1A
+
+  // Calculate and set the OCR1A value based on the desired baud rate
+  OCR1A = (F_CPU / 8) / BAUD_RATE - 1;
+  delay(1000);
 }
 
-void sendSerialByte(char data) {
-  //Start bit
-  digitalWrite(digitalTxPin, LOW);
-  delayMicroseconds(bitDelay);
+void sendStartBit() {
+  Serial.println("Sending start bit");
+  digitalWrite(TX_PIN, LOW);
+  TCNT1 = 0; // Reset Timer1 counter
+  TIFR1 |= (1 << OCF1A); // Clear the interrupt flag
+  sei(); // Enable global interrupts
+}
 
-  // Data bits
-  int numberOfOnes = 0;
-  for (byte mask = 0x01; mask != 0; mask <<= 1) {
-    if (data & mask) {
-      digitalWrite(digitalTxPin, HIGH);
-      numberOfOnes++;
-    } else {
-      digitalWrite(digitalTxPin, LOW);
-    }
-    delayMicroseconds(bitDelay);
-  }
+void sendStopBit() {
+  Serial.println("Sending stop bit");
+  digitalWrite(TX_PIN, HIGH);
+  transmissionInProgress = false;
+}
 
-  // Parity bit
+void sendParityBit() {
   if (numberOfOnes % 2 == 0) {
-    digitalWrite(digitalTxPin, LOW); // Even parity
-    delayMicroseconds(bitDelay);
+    digitalWrite(TX_PIN, LOW);
+  } else {
+    digitalWrite(TX_PIN, HIGH);
   }
-  else {
-    digitalWrite(digitalTxPin, HIGH); // Odd parity
-    delayMicroseconds(bitDelay);
-  }
-
-  // Stop bit
-  digitalWrite(digitalTxPin, HIGH);
 }
 
 void loop() {
-  sendSerialByte(asciiLetter); // Send the ASCIIletter each second
   delay(1000);
+  if (!transmissionInProgress) {
+    transmissionInProgress = true;
+    bitToSend = 0;
+    byteToSend = 'A';
+    sendStartBit();
+  }
+}
+
+ISR(TIMER1_COMPA_vect) {
+  if (bitToSend == 0) {
+    // Start bit has been sent, proceed with the data bits
+    TCNT1 = 0; // Reset Timer1 counter
+  } else if (bitToSend >= 1 && bitToSend <= 8) {
+    // Send the current data bit
+    if (byteToSend & (1 << (bitToSend - 1))) {
+      digitalWrite(TX_PIN, HIGH);
+      numberOfOnes++;
+    } else {
+      digitalWrite(TX_PIN, LOW);
+    }
+    TCNT1 = 0; // Reset Timer1 counter
+  } else if (bitToSend == 9) {
+    // Send parity bit
+    sendParityBit();
+    TCNT1 = 0; // Reset Timer1 counter
+  } else if (bitToSend == 10) {
+    // Send stop bit
+    sendStopBit();
+    cli(); // Disable global interrupts
+  }
+  bitToSend++;
 }
--- Projeto-6-Server/src/main.cpp
+++ Projeto-6-Server/src/main.cpp
@@ -1,80 +1,72 @@
 #include <Arduino.h>
+#define RX_PIN 5
+#define BAUD_RATE 9600
+#define BIT_PERIOD (1000000 / BAUD_RATE)
 
-// Constants
-const int digitalRxPin = 6; // Digital pin that emulates the Tx pin
-const long baudRate = 9600;   // Chose any baud rate you want
-const long bitDelay = 1000000L / baudRate; // Calculate the delay between bits
+volatile uint8_t bitToRead = 0;
+volatile uint8_t receivedByte = 0;
+volatile uint8_t parityBit = 0;
+volatile bool receptionInProgress = false;
+volatile bool isValid = true;
+
+void startReception() {
+  if (!receptionInProgress) {
+    receptionInProgress = true;
+    bitToRead = 0;
+    TCNT1 = 0; // Reset Timer1 counter
+    TIFR1 |= (1 << OCF1A); // Clear the interrupt flag
+  }
+}
 
 void setup() {
   Serial.begin(9600);
-  pinMode(digitalRxPin, INPUT); // Sets the digital pin as an output to emulate the Rx pin
+  pinMode(RX_PIN, INPUT);
+  
+  // Configure Timer1 for CTC (Clear Timer on Compare) mode and enable interrupt on OCR1A match
+  TCCR1A = 0;
+  TCCR1B = (1 << WGM12) | (1 << CS11); // Set CTC mode, prescaler = 8
+  TIMSK1 = (1 << OCIE1A); // Enable compare match interrupt for OCR1A
+
+  // Calculate and set the OCR1A value based on the desired baud rate
+  OCR1A = (F_CPU / 8) / BAUD_RATE - 1;
+  
+  attachInterrupt(digitalPinToInterrupt(RX_PIN), startReception, FALLING);
+  sei(); // Enable global interrupts
 }
 
-char receiveData() {
-  // Looping until the start bit is received
-  while (digitalRead(digitalRxPin) == HIGH) {
-    delayMicroseconds(bitDelay);
-  }
-
-  // Data bits
-  char data = 0;
-  int numberOfOnes = 0;
-  for (byte mask = 0x01; mask != 0; mask <<= 1) {
-    delayMicroseconds(bitDelay);
-    if (digitalRead(digitalRxPin) == HIGH) {
-      data |= mask;
-      numberOfOnes++; 
+void loop() {
+  if (receptionInProgress) {
+    if (bitToRead == 10) {
+      // Reception completed
+      receptionInProgress = false;
+      if (!isValid) {
+        // The received byte is unvalid
+        Serial.println("Invalid byte received");
+      }
+      Serial.print("Received byte: " + String(receivedByte));
+      receivedByte = 0;
+      bitToRead = 0;
+      parityBit = 0;
     }
   }
+}
 
-  // Parity bit
-  delayMicroseconds(bitDelay);
-  if (digitalRead(digitalRxPin) == HIGH && numberOfOnes % 2 == 0) {
-    Serial.println("Parity error");
+ISR(TIMER1_COMPA_vect) {
+  if (receptionInProgress) {
+    if (bitToRead >= 1 && bitToRead <= 8) {
+      // Read the current data bit
+      if (digitalRead(RX_PIN)) {
+        receivedByte |= (1 << (bitToRead - 1));
+        parityBit++;
+      }
+    } else if (bitToRead == 9) {
+      // Read the parity bit
+      if (digitalRead(RX_PIN) && (parityBit % 2 == 0)) {
+        isValid = false;
+      }
+    }
+    bitToRead++;
+
+    TCNT1 = 0; // Reset Timer1 counter
   }
-
-  // Stop bit
-  delayMicroseconds(bitDelay);
-  if (digitalRead(digitalRxPin) == HIGH) {
-    return data;
-  }
-  return 'e';
 }
-
-void loop() {
-  char receivedByte = receiveData();
-  if (receivedByte != 'e') {
-    Serial.println(receivedByte);
-  }
-  delay(1000);
-}
-
-/* Ref: https://electronoobs.com/eng_arduino_tut140.php
-Calculations for the timer: 
-  System clock 16 Mhz and Prescalar 64;
-  Timer 1 speed = 16Mhz/256 = 250 Khz    
-  Pulse time = 1/250 Khz =  4us  
-  Count up to = (1/bitDelay) / 4us = ?
-
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(digitalRxPin, INPUT); // Sets the digital pin as an output to emulate the Rx pin
-  cli(); // Disable interrupts 
-  TCCR1A = 0;                 
-  TCCR1B = 0;                 
-  TCCR1B |= B00000011;      
-  TIMSK1 |= B00000010;
-  OCR1A = bitDelay/4e-6;
-  sei();  //allow interrupts        
-}
-
-void loop() {
-....
-}
-
-ISR(TIMER1_COMPA_vect){
-  TCNT1  = 0; //Reset the timer for next interrupt
-  ...
-}
-*/ 
