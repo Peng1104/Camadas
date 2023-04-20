29649322f75152c580f6432779c919995fb0b40b
Peng1104 <admin@peng1104.net>
2023-04-20 18:33:38 -0300

    Deixando codigo claro

--- Projeto-6-Client/src/main.cpp
+++ Projeto-6-Client/src/main.cpp
@@ -1,60 +1,68 @@
 #include <Arduino.h>
 
 // Constants
-const int digitalTxPin = 5; // Digital pin that emulates the Tx pin
-const char asciiLetter = 'A'; // Choose any letter from the ASCII table to send
-const long baudRate = 9600;   // Chose any baud rate you want
-const long bitDelay = 1000000L / baudRate; // Calculate the delay between bits
+const int OUTPUT_PIN = 5;         // Digital pin that emulates the Tx pin
+const int BAUD_RATE = 9600;       // Chose any baud rate you want
+const long CPU_CLOCK = 21000000; // The arduino cpu clock, 21 MHz
+const long DELAY = CPU_CLOCK/BAUD_RATE;  // Delay in milliseconds
 
-void setup() {
-  pinMode(digitalTxPin, OUTPUT); // Sets the digital pin as an output to emulate the Tx pin
-  digitalWrite(digitalTxPin, HIGH); // Sets the starting value of the digital pin to HIGH
+void setup()
+{
+  pinMode(OUTPUT_PIN, OUTPUT);    // Sets the digital pin as an output to emulate the Tx pin
+  digitalWrite(OUTPUT_PIN, HIGH); // Sets the starting value of the digital pin to HIGH
 }
 
-void sendSerialByte(char data) {
-  //Start bit
-  digitalWrite(digitalTxPin, LOW);
-  for (size_t i = 0; i < 2187; i++){
+void for_delay(double mult)
+{
+  for (int i = 0; mult * DELAY > i; i++)
+  {
     asm("nop");
   }
+}
 
+void sendSerialByte(char data)
+{
+  // Start bit
+  digitalWrite(OUTPUT_PIN, LOW);
+
+  for_delay(1);
 
   // Data bits
-  int numberOfOnes = 0;
-  for (byte mask = 0x01; mask != 0; mask <<= 1) {
-    if (data & mask) {
-      digitalWrite(digitalTxPin, HIGH);
-      numberOfOnes++;
-    } else {
-      digitalWrite(digitalTxPin, LOW);
+  int counter = 0;
+
+  for (byte mask = 0x01; mask != 0; mask <<= 1)
+  {
+    if (data & mask)
+    {
+      digitalWrite(OUTPUT_PIN, HIGH);
+      counter++;
     }
-    for (size_t i = 0; i < 2187; i++){
-      asm("nop");
+    else
+    {
+      digitalWrite(OUTPUT_PIN, LOW);
     }
+    for_delay(1);
   }
 
   // Parity bit
-  if (numberOfOnes % 2 == 0) {
-    digitalWrite(digitalTxPin, LOW); // Even parity
-    for (size_t i = 0; i < 2187; i++){
-      asm("nop");
-    }
+  if (counter % 2 == 0)
+  {
+    digitalWrite(OUTPUT_PIN, LOW); // Even parity
+    for_delay(1);
   }
-  else {
-    digitalWrite(digitalTxPin, HIGH); // Odd parity
-    for (size_t i = 0; i < 2187; i++){
-      asm("nop");
-    }
+  else
+  {
+    digitalWrite(OUTPUT_PIN, HIGH); // Odd parity
+    for_delay(1);
   }
 
   // Stop bit
-  digitalWrite(digitalTxPin, HIGH);
-  for (size_t i = 0; i < 2187; i++){
-    asm("nop");
-  }
+  digitalWrite(OUTPUT_PIN, HIGH);
+  for_delay(1);
 }
 
-void loop() {
-  sendSerialByte(asciiLetter); // Send the ASCIIletter each second
-  delay(2000);
+void loop()
+{
+  sendSerialByte('P'); // Send the ASCIIletter each 2 seconds
+  delay(2000); 
 }
--- Projeto-6-Server/src/main.cpp
+++ Projeto-6-Server/src/main.cpp
@@ -1,54 +1,67 @@
 #include <Arduino.h>
 
 // Constants
-const int digitalRxPin = 6; // Digital pin that emulates the Tx pin
-const long baudRate = 9600;   // Chose any baud rate you want
-const long bitDelay = 1000000L / baudRate; // Calculate the delay between bits
+const int INPUT_PIN = 6;                  // Digital pin that emulates the Tx pin
+const int BAUD_RATE = 9600;               // Chose any baud rate you want
+const long CPU_CLOCK = 21000000;          // The arduino cpu clock, 21 MHz
+const long DELAY = CPU_CLOCK / BAUD_RATE; // Delay in milliseconds
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(digitalRxPin, INPUT); // Sets the digital pin as an output to emulate the Rx pin
+  pinMode(INPUT_PIN, INPUT); // Sets the digital pin as an output to emulate the Rx pin
 }
 
-char receiveData() {
-  // Looping until the start bit is received
-  while (digitalRead(digitalRxPin) == HIGH) {
-  }
-
-  for (size_t i = 0; i < 3280; i++){
+void for_delay(double mult)
+{
+  for (int i = 0; mult * DELAY > i; i++)
+  {
     asm("nop");
   }
+}
+
+char receiveData()
+{
+  // Looping until the start bit is received
+  while (digitalRead(INPUT_PIN) == HIGH)
+  {
+  }
+  for_delay(1.5); // Delay for 1.5 bits
+
   // Data bits
   char data = 0;
-  int numberOfOnes = 0;
-  for (byte mask = 0x01; mask != 0; mask <<= 1) {
-    if (digitalRead(digitalRxPin) == HIGH) {
+  int counter = 0;
+
+  for (byte mask = 0x01; mask != 0; mask <<= 1)
+  {
+    if (digitalRead(INPUT_PIN) == HIGH)
+    {
       data |= mask;
-      numberOfOnes++; 
+      counter++;
     }
-    for (size_t i = 0; i < 2187; i++){
-      asm("nop");
-    }
+    for_delay(1);
   }
 
   // Parity bit
-  if (digitalRead(digitalRxPin) == HIGH && numberOfOnes % 2 == 0) {
+  if (digitalRead(INPUT_PIN) == HIGH && counter % 2 == 0)
+  {
     Serial.println("Parity error");
   }
-  for (size_t i = 0; i < 2187; i++){
-    asm("nop");
-  }
+  for_delay(1);
 
   // Stop bit
-  if (digitalRead(digitalRxPin) == HIGH) {
+  if (digitalRead(INPUT_PIN) == HIGH)
+  {
     return data;
   }
-  return 'e';
+  return (char) 0;
 }
 
-void loop() {
-  char receivedByte = receiveData();
-  if (receivedByte != 'e'){
-    Serial.println(receivedByte);
+void loop()
+{
+  int data = receiveData();
+  
+  if (data > 0) {
+    Serial.println((char) data);
   }
 }
