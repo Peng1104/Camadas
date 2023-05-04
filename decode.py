0c2499e03a50e6b84ab22844bcb8a1d095105dff
Vacbo <peportella11@gmail.com>
2023-05-04 20:04:54 -0300

    Decode v1 done

--- decode.py
+++ decode.py
@@ -1,13 +1,13 @@
-#Importe todas as bibliotecas
+# Importe todas as bibliotecas
 from suaBibSignal import *
-import peakutils 
+import peakutils
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
 import time
 import itertools
 
-#Configs and constants
+# Configs and constants
 KEYS = {"1": [697, 1209], "2": [697, 1336], "3": [697, 1477], "A": [697, 1633], "4": [770, 1209], "5": [770, 1336], "6": [770, 1477], "B": [
     770, 1633], "7": [852, 1209], "8": [852, 1336], "9": [852, 1477], "C": [852, 1633], "X": [941, 1209], "0": [941, 1336], "#": [941, 1477], "D": [941, 1633]}
 SAMPLE_RATE = 48000
@@ -18,14 +18,15 @@ S = signalMeu()
 sd.default.samplerate = SAMPLE_RATE
 sd.default.channels = 1
 
+
 def main():
 
-    #Record audio
+    # Record audio
     audio = sd.rec(int(DURATION * SAMPLE_RATE), SAMPLE_RATE, channels=1)
     sd.wait()
     print("Audio sample recorded")
 
-    #flatten audio list
+    # flatten audio list
     audioFlat = list(itertools.chain(*audio))
 
     print("Plotando audio no tempo")
@@ -39,18 +40,32 @@ def main():
     S.plotFFT(audioFlat, SAMPLE_RATE)
     plt.xlim(500, 1800)
     plt.show()
-   
+
+    #Calculate FFT
     xf, yf = S.calcFFT(audioFlat, SAMPLE_RATE)
 
+    #Get Frequency peaks
     index = peakutils.indexes(yf, thres=0.1, min_dist=50)
-    print("index de picos {}" .format(index))
     print("Frequencias detectadas: {}" .format(xf[index]))
-    filtered_f = [x for x in xf[index] if x <= 2000 and x>=500]
-    print(filtered_f)
+    filtered_f = set(
+        map(int, [x for x in xf[index] if x <= 2000 and x >= 500]))
+    
+    # Decide which key was pressed
+    for key, values in KEYS.items():
+        match_count = 0
 
+        for value in values:
+            if filtered_f.intersection(range(value-10, value+10)):
+                match_count += 1
 
+        if match_count == len(values):
+            print(f"A Key pressionada é: {key}")
+            return
 
+    #Can't detect
+    print("Não foi possivel detectar uma key, tente novamenete")
     return
+
 
 if __name__ == "__main__":
     main()
